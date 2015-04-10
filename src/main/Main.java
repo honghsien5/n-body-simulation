38cfe5e97fb4f9ea99f161ad07a5f76ce957310a
Shien Hong <honghsien5@gmail.com>
2015-04-09 22:04:29 -0700

    added force/movebody pseudocode

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,13 +8,14 @@ import java.io.IOException;
 class Particle{
     double x,y;
     double velocityX,velocityY;
-
+    double forceX,forceY;
     Particle(double x, double y){
         this.x=x;
         this.y=y;
         velocityX = 0 ;
         velocityY = 0;
-
+        forceX = 0;
+        forceY = 0;
     }
 
 
@@ -22,9 +23,45 @@ class Particle{
 
 
 public class Main {
+    static double G = 6.67*Math.pow(10,-11);
+    static double mass = 1;
     static void particlePrint(Particle[] bodies){
         for(int i = 0 ; i < bodies.length;i++){
             System.out.println(i+" is at "+bodies[i].x+" "+bodies[i].y);
+        }
+    }
+
+    static void calculateForces(Particle[] bodies){
+        double distance, magnitude;
+        Particle direction;
+
+        for(int i = 0; i < bodies.length-1;i++){
+            for(int j = i+1; j < bodies.length ; j++){
+                distance = Math.sqrt(Math.pow((bodies[i].x-bodies[j].x),2) + Math.power((bodies[i].y-bodies[j].y),2));
+                magnitude = (G*1*1)/Math.pow(distance,2);
+                direction = new Particle(bodies[j].x-bodies[i].x,bodies[j].y-bodies[i].y);
+                bodies[i].forceX = bodies[i].forceX + magnitude*direction.x/distance;
+                bodies[j].forceX = bodies[j].forceX - magnitude*direction.x/distance;
+                bodies[i].forceY = bodies[i].forceY + magnitude*direction.y/distance;
+                bodies[j].forceY = bodies[j].forceY - magnitude*direction.y/distance;
+
+            }
+        }
+    }
+
+    static void moveBodies(Particle[] bodies, double deltaT){
+        Particle deltaV;
+        Particle deltaP;
+        for(int i = 0 ; i<n;i++){
+            deltaV = new Particle(bodies[i].forceX/1 * deltaT, bodies[i].forceY/1 * deltaT);
+            deltaP = new Particle(bodies[i].velocityX+deltaV.x/2)*deltaT, bodies[i].velicotyY+deltaV..y/2)*deltaT);
+
+            bodies[i].velocityX = bodies[i].velocityX + deltaV.x;
+            bodies[i].velocityY = bodies[i].velocityY + deltaV.y;
+            bodies[i].x = bodies[i].x + deltaP.x;
+            bodies[i].y = bodies[i].y + deltaP.y;
+            bodies[i].forceX = 0;
+            bodies[i].forceY= 0;
         }
     }
     public static void main(String[] args) {
@@ -45,6 +82,8 @@ public class Main {
         String fileName = "init.txt";
         String line;
         double tempX,tempY;
+
+        //input file with init location
         try{
 
 
@@ -61,14 +100,21 @@ public class Main {
             System.out.println(
                     "Unable to open file '" +
                             fileName + "'");
-        }
-        catch(IOException ex) {
+        }catch(IOException ex) {
             System.out.println(
                     "Error reading file '"
                             + fileName + "'");
             // Or we could just do this:
             // ex.printStackTrace();
         }
+
+
+        for(int i = 0 ; i < numTimeSteps;i++){
+
+        }
+
+
+
 
 //        particlePrint(bodies);
 
