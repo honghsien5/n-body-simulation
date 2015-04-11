7cbaebc8899d5b5cc1a7d654725ffb3852261fe0
Shien Hong <honghsien5@gmail.com>
2015-04-10 20:47:39 -0700

    force and move bodies working. detect collision working. working on calculate collision

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,7 @@ class Particle{
     double x,y;
     double velocityX,velocityY;
     double forceX,forceY;
+    double radius;
     Particle(double x, double y){
         this.x=x;
         this.y=y;
@@ -17,15 +18,29 @@ class Particle{
         forceX = 0;
         forceY = 0;
     }
+    Particle(double x, double y, double radius){
+        this.x=x;
+        this.y=y;
+        velocityX = 0 ;
+        velocityY = 0;
+        forceX = 0;
+        forceY = 0;
+        this.radius = radius;
+    }
+    boolean intersect(Particle another){
 
+        return (Math.pow((another.x-x),2) + Math.pow((y-another.y),2)) <= Math.pow((radius+another.radius),2);
+
+    }
 
 }
 
 
 public class Main {
     static double G = 6.67*Math.pow(10,-11);
-    static double mass = 1;
-    static void particlePrint(Particle[] bodies){
+    static double mass = 16000;
+    static int collisionCount = 0;
+    static void printBodies(Particle[] bodies){
         for(int i = 0 ; i < bodies.length;i++){
             System.out.println(i+" is at "+bodies[i].x+" "+bodies[i].y);
         }
@@ -34,11 +49,10 @@ public class Main {
     static void calculateForces(Particle[] bodies){
         double distance, magnitude;
         Particle direction;
-
         for(int i = 0; i < bodies.length-1;i++){
             for(int j = i+1; j < bodies.length ; j++){
-                distance = Math.sqrt(Math.pow((bodies[i].x-bodies[j].x),2) + Math.power((bodies[i].y-bodies[j].y),2));
-                magnitude = (G*1*1)/Math.pow(distance,2);
+                distance = Math.sqrt(Math.pow((bodies[i].x-bodies[j].x),2) + Math.pow((bodies[i].y-bodies[j].y),2));
+                magnitude = (G*mass*mass)/Math.pow(distance,2);
                 direction = new Particle(bodies[j].x-bodies[i].x,bodies[j].y-bodies[i].y);
                 bodies[i].forceX = bodies[i].forceX + magnitude*direction.x/distance;
                 bodies[j].forceX = bodies[j].forceX - magnitude*direction.x/distance;
@@ -52,9 +66,10 @@ public class Main {
     static void moveBodies(Particle[] bodies, double deltaT){
         Particle deltaV;
         Particle deltaP;
-        for(int i = 0 ; i<n;i++){
-            deltaV = new Particle(bodies[i].forceX/1 * deltaT, bodies[i].forceY/1 * deltaT);
-            deltaP = new Particle(bodies[i].velocityX+deltaV.x/2)*deltaT, bodies[i].velicotyY+deltaV..y/2)*deltaT);
+
+        for(int i = 0 ; i < bodies.length;i++){
+            deltaV = new Particle(bodies[i].forceX/mass * deltaT, bodies[i].forceY/mass * deltaT);
+            deltaP = new Particle((bodies[i].velocityX+deltaV.x/2)*deltaT, (bodies[i].velocityY+deltaV.y/2)*deltaT);
 
             bodies[i].velocityX = bodies[i].velocityX + deltaV.x;
             bodies[i].velocityY = bodies[i].velocityY + deltaV.y;
@@ -63,6 +78,30 @@ public class Main {
             bodies[i].forceX = 0;
             bodies[i].forceY= 0;
         }
+    }
+    static void detectCollision(Particle[] bodies){
+        for(int i = 0; i < bodies.length-1;i++){
+            for(int j = i+1; j < bodies.length;j++){
+                if(bodies[i].intersect(bodies[j])){
+                    collisionCount++;
+                    calculateCollision(bodies[i],bodies[j]);
+                }
+            }
+
+        }
+    }
+    static void calculateCollision(Particle p1, Particle p2){
+        double x1  = p1.x;
+        double y1  = p1.y;
+        double v1x = p1.velocityX;
+        double v1y = p1.velocityY
+        double x2  = p2.x;
+        double y2  = p2.y;
+        double v2x = p2.velocityX;
+        double v2y = p2.velocityY;
+//        p1.velocityX = (v2x*Math.pow((x2-x1),2)+v2y*(x2-x1)*(y2-y1)+v1x*Math.pow((y2-y1),2) - v1y*(x2-x1)*(y2-y1))/(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+//        p1.velocityY = (v2x*(x2-x1)*(y2-y1)+v2y*Math.pow(y2-y1,2) - v1x*(y2-y1)*(x2-x1) + v1y*Math.pow(x2-x1,2))/(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+//        p2.velocityX = ()
     }
     public static void main(String[] args) {
 	// write your code here
@@ -73,7 +112,7 @@ public class Main {
 
 
         int numBodies=Integer.parseInt(args[0]);
-        int sizeBodies = Integer.parseInt(args[1]);
+        int radiusBodies = Integer.parseInt(args[1]);
         int numTimeSteps = Integer.parseInt(args[2]);
         int sizeTimeSteps = Integer.parseInt(args[3]);
         long startTime = System.currentTimeMillis();
@@ -93,7 +132,7 @@ public class Main {
             for(int i = 0 ; i < numBodies ; i++){
                 line = bufferedReader.readLine();
                 String[] tempString = line.split(" ");
-                bodies[i] = new Particle(Double.parseDouble(tempString[0]),Double.parseDouble(tempString[1]));
+                bodies[i] = new Particle(Double.parseDouble(tempString[0]),Double.parseDouble(tempString[1]),radiusBodies);
             }
             bufferedReader.close();
         }catch(FileNotFoundException ex) {
@@ -110,8 +149,15 @@ public class Main {
 
 
         for(int i = 0 ; i < numTimeSteps;i++){
-
+            calculateForces(bodies);
+            moveBodies(bodies, sizeTimeSteps);
+            detectCollision(bodies);
+            System.out.println("Time: " + i*sizeTimeSteps +" Collision Count: " + collisionCount);
+            printBodies(bodies);
         }
+
+
+
 
 
 
