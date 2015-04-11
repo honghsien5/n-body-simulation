4b6f9e935b114967cfdb052e27437f9b2fac7dd8
honghsien5 <honghsien5@gmail.com>
2015-04-11 16:29:55 -0700

    Completed sequential portion of java. added output file and disabled debug texts Init.txt only has 4 entries so it need to be adjusted later

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,18 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
+/**
+ * Particle class used for storing particle's information as well as testing if one interact with another
+ */
 class Particle{
     int id;
     double x,y;
     double velocityX,velocityY;
     double forceX,forceY;
     double radius;
-    Particle(double x, double y){
-        this.x=x;
-        this.y=y;
-        velocityX = 0 ;
-        velocityY = 0;
-        forceX = 0;
-        forceY = 0;
-    }
+
+    //constructor for the particle creation
     Particle(int id,double x, double y, double radius){
         this.id = id;
         this.x=x;
@@ -29,25 +23,38 @@ class Particle{
         forceY = 0;
         this.radius = radius;
     }
+
+    //constructor for placeholder particle
+    Particle(double x, double y){
+        this.x=x;
+        this.y=y;
+        velocityX = 0 ;
+        velocityY = 0;
+        forceX = 0;
+        forceY = 0;
+    }
+
+    //use the radius and position to calculate if this particle intersect with another particle
     boolean intersect(Particle another){
-
         return (Math.pow((another.x-x),2) + Math.pow((y-another.y),2)) <= Math.pow((radius+another.radius),2);
-
     }
 
 }
 
 
 public class Main {
+    //initializing constants and variables
     static double G = 6.67*Math.pow(10,-11);
     static double mass = 1;
     static int collisionCount = 0;
+
+    //method for printing all particles' position
     static void printBodies(Particle[] bodies){
         for(int i = 0 ; i < bodies.length;i++){
             System.out.println(i+" is at "+bodies[i].x+" "+bodies[i].y);
         }
     }
-
+    //method for calculating the forces acting on each particle
     static void calculateForces(Particle[] bodies){
         double distance, magnitude;
         Particle direction;
@@ -60,12 +67,10 @@ public class Main {
                 bodies[j].forceX = bodies[j].forceX - magnitude*direction.x/distance;
                 bodies[i].forceY = bodies[i].forceY + magnitude*direction.y/distance;
                 bodies[j].forceY = bodies[j].forceY - magnitude*direction.y/distance;
-//                System.out.println("Force 1: " + bodies[i].forceX +" " + bodies[i].forceY);
-//                System.out.println("Force 2: " + bodies[j].forceX + " " + bodies[j].forceY);
             }
         }
     }
-
+    //method all the particles according to its force and velocity
     static void moveBodies(Particle[] bodies, double deltaT){
         Particle deltaV;
         Particle deltaP;
@@ -76,13 +81,13 @@ public class Main {
 
             bodies[i].velocityX = bodies[i].velocityX + deltaV.x;
             bodies[i].velocityY = bodies[i].velocityY + deltaV.y;
-            System.out.println("Velocity "+ i + ": " + bodies[i].velocityX +" " + bodies[i].velocityY);
             bodies[i].x = bodies[i].x + deltaP.x;
             bodies[i].y = bodies[i].y + deltaP.y;
             bodies[i].forceX = 0;
             bodies[i].forceY= 0;
         }
     }
+    //detect collision for all particles and calculate the velocity values of each colliding particle
     static void detectCollision(Particle[] bodies){
         for(int i = 0; i < bodies.length-1;i++){
             for(int j = i+1; j < bodies.length;j++){
@@ -94,19 +99,31 @@ public class Main {
 
         }
     }
+    //detect if any of the particle hits the boundary and change the direction where the particle goes
     static void detectBoundary(Particle[] bodies){
         for(int i = 0; i < bodies.length;i++){
             double x = bodies[i].x;
             double y = bodies[i].y;
-            if(x < -100 || x > 100){
+            if(x < -100){
+                System.out.println(bodies[i].id + " hits the left boundary and bounced.");
+                bodies[i].velocityX = -bodies[i].velocityX;
+            }else if(x> 100){
+                System.out.println(bodies[i].id + " hits the right boundary and bounced.");
                 bodies[i].velocityX = -bodies[i].velocityX;
             }
-            if( y < -100 || y > 100){
+
+            if(y < -100){
+                System.out.println(bodies[i].id + " hits the bottom boundary and bounced.");
+                bodies[i].velocityY = -bodies[i].velocityY;
+            }else if(y > 100){
+                System.out.println(bodies[i].id + " hits the top boundary and bounced.");
                 bodies[i].velocityY = -bodies[i].velocityY;
             }
 
         }
     }
+
+    //calculate the velocity after the collision between particle 1 and 2
     static void calculateCollision(Particle p1, Particle p2){
         double x1  = p1.x;
         double y1  = p1.y;
@@ -116,41 +133,48 @@ public class Main {
         double y2  = p2.y;
         double v2x = p2.velocityX;
         double v2y = p2.velocityY;
-        System.out.println("Collision between particle "+ p1.id +" and particle " + p2.id);
-        System.out.println("Before:");
-        System.out.println(p1.velocityX + " " + p1.velocityY);
-        System.out.println(p2.velocityX + " " + p2.velocityY);
+//        System.out.println("Collision between particle "+ p1.id +" and particle " + p2.id);
+//        System.out.println("Velocity Before:");
+//        System.out.println(p1.velocityX + " " + p1.velocityY);
+//        System.out.println(p2.velocityX + " " + p2.velocityY);
         p1.velocityX = (v2x*Math.pow(x2-x1,2) + v2y*(x2-x1)*(y2-y1) + v1x*Math.pow(y2-y1,2) - v1y*(x2-x1)*(y2-y1))/(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
         p1.velocityY = (v2x*(x2-x1)*(y2-y1) + v2y*Math.pow(y2-y1,2) - v1x*(y2-y1)*(x2-x1) + v1y*Math.pow(x2-x1,2))/(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
         p2.velocityX = (v1x*Math.pow(x2-x1,2) + v1y*(x2-x1)*(y2-y1) + v2x*Math.pow(y2-y1,2) - v2y*(x2-x1)*(y2-y1))/(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
         p2.velocityY = (v1x*(x2-x1)*(y2-y1) + v1y*Math.pow(y2-y1,2) - v2x*(y2-y1)*(x2-x1) + v2y*Math.pow(x2-x1,2))/(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
-        System.out.println("after:");
-        System.out.println(p1.velocityX + " " + p1.velocityY);
-        System.out.println(p2.velocityX + " " + p2.velocityY);
-        System.out.println();
+//        System.out.println("Velocity after:");
+//        System.out.println(p1.velocityX + " " + p1.velocityY);
+//        System.out.println(p2.velocityX + " " + p2.velocityY);
+//        System.out.println();
     }
+
     public static void main(String[] args) {
-	// write your code here
+
+        //check for the correct amount of arguments
         if ( args.length < 3){
+            if( args.length > 4){
+                System.out.println("Max argument is 4");
+            }
             System.out.println("Need more than three arguments.");
             return;
         }
 
-
+        //apply arguments
         int numBodies=Integer.parseInt(args[0]);
-        int radiusBodies = Integer.parseInt(args[1]);
+        double radiusBodies = Double.parseDouble(args[1]);
         int numTimeSteps = Integer.parseInt(args[2]);
-        int sizeTimeSteps = Integer.parseInt(args[3]);
-        long startTime = System.currentTimeMillis();
-        long estimatedTime;
+        int sizeTimeSteps;
+        if(args.length == 4){
+            sizeTimeSteps = Integer.parseInt(args[3]);
+        }else{
+            sizeTimeSteps = 5000;
+        }
+
         Particle[] bodies = new Particle[numBodies];
         String fileName = "init.txt";
         String line;
-        double tempX,tempY;
 
         //input file with init location
         try{
-
 
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -169,37 +193,48 @@ public class Main {
             System.out.println(
                     "Error reading file '"
                             + fileName + "'");
-            // Or we could just do this:
-            // ex.printStackTrace();
         }
 
-
+        //start counting time
+        long startTime = System.currentTimeMillis();
+        long estimatedTime;
         for (int i = 0 ; i < numTimeSteps;i++){
             calculateForces(bodies);
             moveBodies(bodies, sizeTimeSteps);
-//            detectBoundary(bodies);
+            detectBoundary(bodies);
             detectCollision(bodies);
-            System.out.println("Time: " + i*sizeTimeSteps +"ms Collision Count: " + collisionCount);
-            printBodies(bodies);
+            //System.out.println("Computation time:Time: " + i*sizeTimeSteps +"ms Collision Count: " + collisionCount);
+           // printBodies(bodies);
+        }
+        estimatedTime = System.currentTimeMillis() - startTime;
+        long seconds = estimatedTime/1000;
+        long milliseconds = estimatedTime-(seconds*1000);
+        System.out.println("computation time:\t" + seconds +" seconds\t"+ milliseconds + " milliseconds");
+        System.out.println("collision count:\t" + collisionCount);
+
+        try{
+            PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+            //continue the operation for time steps
+            for(int i = 0 ; i < bodies.length;i++){
+                writer.println("Particle " + bodies[i].id + ":");
+                writer.println("Position X:\t" + bodies[i].x + "\t\tPosition Y:\t" + bodies[i].y);
+                writer.println("Velocity X:\t" + bodies[i].velocityX + "\tVelocity Y:\t" + bodies[i].velocityY);
+                writer.println();
+            }
+
+            writer.close();
+        }catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                           "output.txt" + "'");
+        }catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + "output.txt" + "'");
         }
 
 
 
-
-
-
-
-//        particlePrint(bodies);
-
-//        while(true){
-//            estimatedTime = System.currentTimeMillis() - startTime;
-//            long seconds = estimatedTime/1000;
-//            long milliseconds = estimatedTime-(seconds*1000);
-//            System.out.println("computation time:" + seconds +" seconds "+ milliseconds + " milliseconds");
-//            if(estimatedTime%sizeTimeSteps==0 ){
-//                System.out.println("computation time:" + seconds +" seconds "+ milliseconds + " milliseconds");
-//            }
-//        }
 
 
     }
